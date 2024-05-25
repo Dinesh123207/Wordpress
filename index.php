4b0579113d80c806fd10327560b2c9f7d5d8c08c
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-05-25 14:38:48 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+function greet($name,$color){
+
+    echo "my name is $name. My fav color is $color";
+}
+greet('dinesh','blue');
+greet('vickey','purple');
+?>
+<h1>Hello guys</h1>
+<h2> <?php  bloginfo( 'name' ); ?> </h2>
+<p> <?php   bloginfo( 'description' ); ?> </p>
+

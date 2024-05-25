0ab35dd296d9dde792047b4ff5d04b94998b6c4d
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-05-25 17:24:27 +0530

    new changes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 function greet($name,$color){
 
     echo "my name is $name. My fav color is $color";
@@ -8,5 +8,18 @@ greet('vickey','purple');
 ?>
 <h1>Hello guys</h1>
 <h2> <?php  bloginfo( 'name' ); ?> </h2>
-<p> <?php   bloginfo( 'description' ); ?> </p>
+<p> <?php   bloginfo( 'description' ); ?> </p> -->
 
+<?php
+
+$names = array('brad', 'john', 'kajal','shruti');
+
+
+
+
+$count =1;
+while($count< 101){
+    echo "<li> $count <li>";
+    $count++;
+}
+?>

e11c0df7a26bd080109cd66c81deb01236cd4659
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-05-26 11:50:20 +0530

    changes

--- single.php
+++ single.php
@@ -1,16 +1,14 @@
 <?php
-while(have_posts()){
-    the_post();?>
+  
+  get_header();
 
+  while(have_posts()) {
+    the_post(); ?>
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
-  
-    <?php      } 
-
-
     
+  <?php }
 
-
-
+  get_footer();
 
 ?>
